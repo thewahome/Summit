ec35a38d00f68b766bc378f8f62e84aae12b4e16
Charles Wahome <thewahome@users.noreply.github.com>
2018-09-26 13:17:34 +0530

    fix failing test

--- Summit.Tests/Controllers/ValuesControllerTest.cs
+++ Summit.Tests/Controllers/ValuesControllerTest.cs
@@ -24,7 +24,7 @@ namespace Summit.Tests.Controllers
 
             // Assert
             Assert.IsNotNull(result);
-            Assert.AreEqual(2, result.Count());
+            Assert.AreEqual(3, result.Count());
             Assert.AreEqual("value1", result.ElementAt(0));
             Assert.AreEqual("value2", result.ElementAt(1));
         }
